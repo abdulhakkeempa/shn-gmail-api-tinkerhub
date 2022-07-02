af96352a3f98b6d102034727fc9de2b09a269f9e
hakkeempa <abdulhakkeempa2002@gmail.com>
2022-07-02 22:31:17 +0530

    some change

--- gmailApi/gmail/views.py
+++ gmailApi/gmail/views.py
@@ -2,12 +2,19 @@ import django
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
+<<<<<<< HEAD
+=======
+#from gmail.models import OTP
+
+>>>>>>> fc819fbdf43c71805e218875b1135b7a2e989170
 from .forms import ProjectForm,OTPForm
 from .sendOTP import *
 
 # Create your views here.
 def homePage(request):
     return render(request,"gmail/webpage_1.html")
+
+global ret_otp
 
 def emailPage(request):
     form = ProjectForm()
@@ -16,12 +23,21 @@ def emailPage(request):
         if form.is_valid():
             form.save()
             InputEmail = form['email'].value()
-            sendotp(InputEmail,"Django - Test")
+            global ret_otp
+            ret_otp = sendotp(InputEmail,"Django - Test")
             return redirect('otpValidation')
     context = {}
     context['form'] = form
     template = 'gmail/webpage_1.html'
     return render(request,template,context)
+
+
+def checkOTP(userOTP,ret_otp):
+    if userOTP == ret_otp:
+        return True
+    else:
+        return False
+
 
 def otpPage(request):
     form = OTPForm()
@@ -30,7 +46,7 @@ def otpPage(request):
         if form.is_valid():
             form.save()
             userOTP = form['otp'].value()
-            if checkOTP(userOTP):
+            if checkOTP(userOTP,ret_otp):
                 return render(request,"gmail/webpage_3.html")
             else:
                 return render(request,"gmail/error.html")
